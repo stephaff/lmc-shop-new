459a5d5d2112a5b613decb67d286493577179ba4
stephaff <affalistephane@gmail.com>
2022-08-09 13:18:25 +0000

    index fifth

--- js/index.js
+++ js/index.js
@@ -15,3 +15,22 @@ function close(){
     const slideBar = document.querySelector('.slide-bar');
     slideBar.classList.toggle('open-slide-bar');
 }
+
+const filtre = document.querySelector('.filter');
+
+filtre.addEventListener('click', openPopular);
+
+function openPopular(){
+    const popular = document.querySelector('.main-left');
+    popular.classList.add('open-popular');   
+}
+
+const closePopular = document.querySelector('.close-popular');
+
+closePopular.addEventListener('click', closePopularWindow);
+
+function closePopularWindow(){
+    const popular = document.querySelector('.main-left');
+    popular.classList.remove('open-popular');   
+}
+
